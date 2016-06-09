fca573aae866528eb158d8bc430480a0437921f7
skyskies0313 <skyskies3@outlook.jp>
2016-06-10 01:28:33 +0900

    getReward
    Reward.csにgetRewardを実装

--- project14_pt2/Assets/Scripts/Reward.cs
+++ project14_pt2/Assets/Scripts/Reward.cs
@@ -2,19 +2,16 @@
 using System.Collections;
 
 public class Reward : MonoBehaviour {
-    private Rigidbody unko;
-    public float reward;
-    void Start () {
-        unko = GetComponent<Rigidbody>();
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        reward = GameObject.Find("Goal").GetComponent<Collider>().bounds.center.z - unko.transform.position.z;
-    }
-    void OnGUI()
-    {
-        string label = reward.ToString();
-        GUI.Label(new Rect(0, 0, 100, 30), label);
+    public static Rigidbody unko;
+    public static double reward;
+    public static byte[] breward;
+
+    public static byte[] getReward () {
+        
+        reward = GameObject.Find("Goal").GetComponent<Collider>().bounds.center.z - GameObject.Find("Car").GetComponent<Collider>().transform.position.z;
+        breward = System.BitConverter.GetBytes(reward);
+
+        return breward;
+           
     }
 }
---
+++ project14_pt2/Assets/Scripts/TestGetReward.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestGetReward : MonoBehaviour {
+    byte[] a;
+	void Update () {
+
+        a = Reward.getReward();
+        if (Input.GetKeyDown("r"))
+            for (int i = 0; i < a.Length; i++)
+            {
+                Debug.Log(a[i]);
+            }
+        if (Input.GetKeyDown("b"))
+            Debug.Log(System.BitConverter.ToDouble(a, 0));
+    }
+}
